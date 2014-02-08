410c56a6f555c9d95ac1702f0b213a7cd7ca3e0b
Viktor Staikov <viktor.staikov@gmail.com>
2014-02-08 19:40:27 +0200

    add page for adding places - fix

--- public/javascripts/addplace.js
+++ public/javascripts/addplace.js
@@ -16,14 +16,16 @@ $(document).ready(function(){
 
       var open = '10:00';
       var close = '18:00';
-      $( "#" + day + '-hours' ).val( open + " - " + close);
+      $( "#" + day + '-hours-open' ).val(open);
+      $( "#" + day + '-hours-close' ).val(close);
     },
     slide: function( event, ui ) {
       var day = $(this)[0].id.split('-')[1];
 
       var open = toDoulbeDigitNumber(Math.floor(ui.values[0]/60)) + ':' + toDoulbeDigitNumber(ui.values[0]%60);
       var close = toDoulbeDigitNumber(Math.floor(ui.values[1]/60)) + ':' + toDoulbeDigitNumber(ui.values[1]%60);
-      $( "#" + day + '-hours' ).val( open + " - " + close);
+      $( "#" + day + '-hours-open' ).val(open);
+      $( "#" + day + '-hours-close' ).val(close);
     }
   });
 
