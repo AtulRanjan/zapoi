b99d81494f5f66e9c4707a7e98a64a6380e71332
Viktor Staikov <viktor.staikov@gmail.com>
2014-02-08 15:26:36 +0200

    map is initialized with yout current location,

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,11 +1,23 @@
 var map;
+
 function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
-  map = new google.maps.Map(document.getElementById('map_canvas'),
-      mapOptions);
+  if( "geolocation" in navigator){
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        var mapOptions = {
+          zoom: 12,
+          center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+        };
+        map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+      },
+      function(err){
+        alert(err);
+      }
+    );
+  }
+  else {
+    alert("suck a dick");
+  }
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
